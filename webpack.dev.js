072cda4563aee9eb80ffe3ad5ac73333325050f9
Damian Sikosana <sikosanadamian@gmail.com>
2022-10-10 07:58:07 +0200

    webpack configurations

---
+++ webpack.dev.js
@@ -0,0 +1,89 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = () => {
+    return {
+        mode: "development",
+        plugins:
+            [new webpack.ProvidePlugin({
+                "React": "react",
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].bundle.css',
+                chunkFilename: '[id].css'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                inject: true,
+                template: path.resolve(__dirname, 'public', 'index.html'),
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].bundle.css',
+                chunkFilename: '[id].css'
+            }),
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify("development"),
+                    REACT_APP_API: JSON.stringify("https://dms-spring.herokuapp.com")
+                }
+            })
+        ],
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            publicPath: '/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    include: path.resolve(__dirname, 'src'),
+                    exclude: /node_modules/,
+                    use: ["style-loader",
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+
+                        "postcss-loader"],
+                },
+                {
+                    test: /\.(jsx|js)$/,
+                    include: path.resolve(__dirname, 'src'),
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    "targets": "defaults"
+                                }],
+                                '@babel/preset-react'
+                            ]
+                        }
+                    }]
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                    include: path.resolve(__dirname, 'src'),
+                    type: 'asset/resource',
+                }
+            ]
+        },
+        devServer: {
+            open: true,
+            port: 9009,
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            historyApiFallback: true,
+            liveReload: true
+        }
+    }
+}
---
+++ webpack.prod.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = () => {
+    return {
+        mode: "production",
+        plugins: [
+            new webpack.ProvidePlugin({
+                "React": "react",
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].bundle.css',
+                chunkFilename: '[id].css'
+            }),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                inject: true,
+                template: path.resolve(__dirname, 'public', 'index.html'),
+                favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].bundle.css',
+                chunkFilename: '[id].css'
+            }),
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify("production"),
+                    REACT_APP_API: JSON.stringify("https://dms-spring.herokuapp.com")
+                }
+            })
+        ],
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            publicPath: '/'
+            },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    include: path.resolve(__dirname, 'src'),
+                    exclude: /node_modules/,
+                    use: ["style-loader",
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+
+                        "postcss-loader"],
+                },
+                {
+                    test: /\.(jsx|js)$/,
+                    include: path.resolve(__dirname, 'src'),
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
+                                    "targets": "defaults"
+                                }],
+                                '@babel/preset-react'
+                            ]
+                        }
+                    }]
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                    include: path.resolve(__dirname, 'src'),
+                    type: 'asset/resource',
+                },
+            ]
+        }
+    }
+}
